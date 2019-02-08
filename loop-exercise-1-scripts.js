9c4dc86778b301dd9cdf5865ae421c446359cf45
Derek <derekdenhartigh@gmail.com>
2019-02-08 15:16:05 -0500

    aesthetics done, working on getting temp readout to count up/down with 1sec delay (not instantaneous) as well as having the console log the comf/cozy message once desired temp is reached

--- loop-exercise-1-scripts.js
+++ loop-exercise-1-scripts.js
@@ -1,1 +1,127 @@
 "use strict";
+/*
+Skills: Conditions
+Heating/Cooling exercise:
+Define two variables: actualTemp and a desiredTemp. Write conditionals to tell the heating &
+cooling system what to do. Log one of these three things: Run A/C, Run heat, or Standby.
+Extended Challenges second exercise:
+Start with two variables: tempCelsius (a number) and targetUnit (a string, either “C”, “F”, or “K”).
+Write a switch statement that checks the targetUnit and logs the temperature converted to
+that unit. Notes: K stands for Kelvin. C requires no conversion, print out the original temp
+*/
+
+
+/* I didn't read the directions...
+let actualTemp = prompt("what is the temperature? (numerical values only please)")
+let desiredTemp = prompt("What temperature do you want it to be? (numerical values only please)")
+
+if (actualTemp === desiredTemp) {console.log("Its comfy in here, get your hand off the thermostat.")
+while (actualTemp<desiredTemp) {
+    actualTemp++, console.log(actualTemp)};
+while (actualTemp>desiredTemp) {
+    desiredTemp--, console.log(actualTemp);
+}
+*/
+/*
+while (actualTemp<desiredTemp) {
+    console.log(`Current Temperature: ${actualTemp}`)
+    actualTemp++;
+    console.log("Run Heat")
+    console.log(`Modified Temperature: ${actualTemp}, It's getting hot in here`)
+    // if (actualTemp===desiredTemp) // this doesn't do anything because the actualTemp variable isn't globally being incremented, only locally, so the condition is never met
+    // console.log("Tis relatively comfy & cozy now.");
+};
+while (actualTemp>desiredTemp) {
+    console.log(`Current Temperature: ${actualTemp}`)
+    actualTemp--;
+    console.log("Run A/C");
+    console.log(`Modified Temperature: ${actualTemp}, ICE ICE BABY`)
+    // if (actualTemp===desiredTemp)    
+    // console.log("Tis relatively comfy & cozy now.");
+};
+if (actualTemp===desiredTemp)    
+    console.log("Tis relatively comfy & cozy already.\nSTANDBY SOLDIER, that's an order!");
+*/
+
+//this works, but, is no better and a little worse...
+// if (actualTemp<desiredTemp) {
+//     while (actualTemp<desiredTemp){
+//         console.log(`Current Temperature: ${actualTemp}`)
+//         actualTemp++;
+//         console.log("Run Heat")
+//         console.log(`Modified Temperature: ${actualTemp}, It's getting hot in here`)}
+//     }
+// else {
+//     console.log("Tis relatively comfy & cozy now.");}  // I thought maybe the else would run after the if condition wasn't met, no go...
+
+// while (actualTemp>desiredTemp) {
+//     console.log(`Current Temperature: ${actualTemp}`)
+//     actualTemp--;
+//     console.log("Run A/C");
+//     console.log(`Modified Temperature: ${actualTemp}, ICE ICE BABY`)
+//     if (actualTemp===desiredTemp)    
+//     console.log("Tis relatively comfy & cozy now.");
+// };
+// if (actualTemp===desiredTemp)    
+//     console.log("Tis relatively comfy & cozy already.\nSTANDBY SOLDIER, that's an order!");
+
+    // tried having comfycozy message as an if statement following while loops, but it never logged it, tried embedding if statements
+    // at the end of loops, same issue.
+    // gonna try embedding a loop in an if/else statement - its really not working, I think it is because the 
+    // Temp variables never change outside of the loop once they are globally defined outside the loops by the prompts
+    // am I getting somewhere?  shall I define the variables within the loops and fix this issue.. not sure how to do that..
+
+// lets try this one more time with feeling:
+
+function thermostatFunction(actualTemp, desiredTemp) {
+let actualTemp=prompt("What is the current temperature?")
+let desiredTemp=prompt("What temperature do you want it to be?")
+if (actualTemp<desiredTemp) {
+    while (actualTemp<desiredTemp){
+        console.log(`Current Temperature: ${actualTemp}`)
+        actualTemp++;
+        console.log("Run Heat");
+        console.log(`Modified Temperature: ${actualTemp}, It's getting hot in here`)}}
+else if (actualTemp>desiredTemp) {
+    while (actualTemp>desiredTemp){
+        console.log(`Current Temperature: ${actualTemp}`)
+        actualTemp--;
+        console.log("Run A/C");
+        console.log(`Modified Temperature: ${actualTemp}, ICE ICE BABY`)}}
+else if (actualTemp===desiredTemp){
+    console.log(`Take your finger off that dial son, it already ${actualTemp}!\nSTANDBY SOLDIER, that's an order!`)
+}
+else {console.log("It's relatively comfy cozy in here now.")};
+}
+// got this one to work, but the else statement that I was hoping would be my endpoint after the loop isn't running.
+//to do later: maybe drop a thermostat image, position a button on it or forms, and have scripts display currentTemp in DOM with a 1.5 sec delay in loops
+
+
+/*
+Extended Challenge TIME:
+Start with two variables: tempCelsius (a number) and targetUnit (a string, either “C”, “F”, or “K”).
+Write a switch statement that checks the targetUnit and logs the temperature converted to
+that unit. Notes: K stands for Kelvin. C requires no conversion, print out the original temp
+*/
+
+let actualTemp = prompt("What temperature is it?");
+let desiredTemp = prompt("What temperature do you want it?");
+
+/* example while loop
+let a=0, j=0;
+while (a<30) {
+    a++;
+    j+=a;
+    console.log(`a: ${a}`);
+    console.log(`j: ${j}`);
+
+do while:
+let userPass = undefined;
+do {
+    if (userPass===undefined) {console.log("Enter Password to continue")
+    userPass = prompt("Enter \"password\"")
+    {console.log("Access Granted")} 
+}
+while (userPass !== "password");
+*/
+
